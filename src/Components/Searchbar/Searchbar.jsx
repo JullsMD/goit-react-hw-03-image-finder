5fa8434d640cf9485b4b0d2bc47b3535928c3ad0
Julia Melnik <julls.hm.md@gmail.com>
2021-07-30 17:36:23 +0300

    ---

---
+++ src/Components/Searchbar/Searchbar.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Searchbar.module.css';
+
+class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.query.trim() === '') {
+      return alert('Where is your query?');
+    }
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
+
+  handleChange = e => {
+    this.setState({
+      query: e.currentTarget.value,
+    });
+  };
+
+  render() {
+    return (
+      <header className={styles.Searchbar}>
+        <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={styles.SearchFormButton}>
+            <span className={styles.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            className={styles.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+            value={this.state.query}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+export default Searchbar;
